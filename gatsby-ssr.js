75966b0686cc61af303ed954acc1c3bdfc7eea5d
Mohit Chauhan <mhchauhan3@gmail.com>
2026-09-20 15:42:39 -0700

    add analytics

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -4,9 +4,21 @@
  * See: https://www.gatsbyjs.com/docs/reference/config-files/gatsby-ssr/
  */
 
+const React = require("react")
+
 /**
  * @type {import('gatsby').GatsbySSR['onRenderBody']}
  */
-exports.onRenderBody = ({ setHtmlAttributes }) => {
+exports.onRenderBody = ({ setHtmlAttributes, setHeadComponents }) => {
   setHtmlAttributes({ lang: `en` })
+
+  setHeadComponents([
+    React.createElement("script", {
+      key: "umami-analytics",
+      defer: true,
+      src: "https://analytics.mohitc.com/script.js",
+      "data-website-id": "f5c51a68-19d3-4c6f-8b44-8a9716c64c1b",
+      "data-domains": "mohitc.com,www.mohitc.com",
+    }),
+  ])
 }
