23d4eda92208d32ddce5fe6b1354fc92c27588e7
Mohit Chauhan <mhchauhan3@gmail.com>
2026-09-20 23:55:22 -0700

    Improve site SEO metadata

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -6,55 +6,50 @@ import Seo from "../components/seo"
 
 const BlogPostTemplate = ({
   data: { previous, next, markdownRemark: post },
-}) => {
-  return (
-    <BlogLayout>
-      <article
-        className="blog-post"
-        itemScope
-        itemType="http://schema.org/Article"
-      >
-        <header>
-          <h1 itemProp="headline">{post.frontmatter.title}</h1>
-          <p className="post-date">{post.frontmatter.date}</p>
-        </header>
-        <section
-          dangerouslySetInnerHTML={{ __html: post.html }}
-          itemProp="articleBody"
-        />
-      </article>
-      {(previous || next) && (
-        <nav className="blog-post-nav" aria-label="More posts">
-          <ul>
-            <li>
-              {previous && (
-                <Link to={`/blog${previous.fields.slug}`} rel="prev">
-                  ← {previous.frontmatter.title}
-                </Link>
-              )}
-            </li>
-            <li>
-              {next && (
-                <Link to={`/blog${next.fields.slug}`} rel="next">
-                  {next.frontmatter.title} →
-                </Link>
-              )}
-            </li>
-          </ul>
-        </nav>
-      )}
-    </BlogLayout>
-  )
-}
+}) => (
+  <BlogLayout>
+    <article className="blog-post" itemScope itemType="https://schema.org/Article">
+      <header>
+        <h1 itemProp="headline">{post.frontmatter.title}</h1>
+        <p className="post-date">{post.frontmatter.date}</p>
+      </header>
+      <section
+        dangerouslySetInnerHTML={{ __html: post.html }}
+        itemProp="articleBody"
+      />
+    </article>
+    {(previous || next) && (
+      <nav className="blog-post-nav" aria-label="More posts">
+        <ul>
+          <li>
+            {previous && (
+              <Link to={`/blog${previous.fields.slug}`} rel="prev">
+                ← {previous.frontmatter.title}
+              </Link>
+            )}
+          </li>
+          <li>
+            {next && (
+              <Link to={`/blog${next.fields.slug}`} rel="next">
+                {next.frontmatter.title} →
+              </Link>
+            )}
+          </li>
+        </ul>
+      </nav>
+    )}
+  </BlogLayout>
+)
 
-export const Head = ({ data: { markdownRemark: post } }) => {
-  return (
-    <Seo
-      title={post.frontmatter.title}
-      description={post.frontmatter.description || post.excerpt}
-    />
-  )
-}
+export const Head = ({ data: { markdownRemark: post } }) => (
+  <Seo
+    title={post.frontmatter.title}
+    description={post.frontmatter.description || post.excerpt}
+    pathname={`/blog${post.fields.slug}`}
+    article
+    datePublished={post.frontmatter.dateISO}
+  />
+)
 
 export default BlogPostTemplate
 
@@ -68,27 +63,21 @@ export const pageQuery = graphql`
       id
       excerpt(pruneLength: 160)
       html
+      fields { slug }
       frontmatter {
         title
         date(formatString: "MMMM DD, YYYY")
+        dateISO: date(formatString: "YYYY-MM-DD")
         description
       }
     }
     previous: markdownRemark(id: { eq: $previousPostId }) {
-      fields {
-        slug
-      }
-      frontmatter {
-        title
-      }
+      fields { slug }
+      frontmatter { title }
     }
     next: markdownRemark(id: { eq: $nextPostId }) {
-      fields {
-        slug
-      }
-      frontmatter {
-        title
-      }
+      fields { slug }
+      frontmatter { title }
     }
   }
 `
